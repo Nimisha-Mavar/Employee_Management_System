2d9fe53d2d465b41ec5fc19f1ccbe9ca98be6c66
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2024-06-16 19:52:01 -0400

    Create employee logic

--- EMS_CRUD/CRUD/views.py
+++ EMS_CRUD/CRUD/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from .models import Employee
+from django.contrib import messages
 # Create your views here.
 def home(request):
     emps=Employee.objects.all()
+    if request.method=='POST':
+        if "add" in request.POST:
+            name=request.POST.get("name")
+            email=request.POST.get("name")
+            role=request.POST.get("role")
+
+            newEmp=Employee.objects.create(
+                name=name,email=email,role=role
+            )
+
+            messages.success(request,"Employee Created Successfully")
     context={
         "Employees":emps
     }
